04b07911374e4a06b23d70b48bd594508da60765
GabiAguirre07 <gabriel.aguirre2007@gmail.com>
2025-06-05 17:21:35 -0300

    iniciando

---
+++ Actividad11/Ej2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Ej2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvresultado = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvresultado
+            // 
+            this.lvresultado.AutoSize = true;
+            this.lvresultado.Location = new System.Drawing.Point(196, 299);
+            this.lvresultado.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lvresultado.Name = "lvresultado";
+            this.lvresultado.Size = new System.Drawing.Size(121, 20);
+            this.lvresultado.TabIndex = 11;
+            this.lvresultado.Text = "label1 resultado";
+            this.lvresultado.Click += new System.EventHandler(this.lvresultado_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(451, 287);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(356, 155);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "CalcularPromedio";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 467);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(721, 200);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 100);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(372, 59);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(405, 183);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 680);
+            this.Controls.Add(this.lvresultado);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnRegistrar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lvresultado;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+    }
+}
+
---
+++ Actividad11/Ej2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej2
+{
+    public partial class Form1 : Form
+    {
+        double acumulador = 0;
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double prom=acumulador/contador;
+            lvresultado.Text=$"El promedio es: {prom:F2}";
+            textBox2.Text = $"El promedio es: {prom:F2}";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            double valor=Convert.ToDouble(textBox1.Text);
+            acumulador += valor;
+            contador++;
+        }
+
+        private void lvresultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
